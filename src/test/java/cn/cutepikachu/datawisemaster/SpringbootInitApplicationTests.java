af41a92168c7271803177972fc5d4a50cf9fafb6
2894462606 <2894462606@qq.com>
2024-05-07 17:16:48 +0800

    test: 对象手动验证测试

--- src/test/java/cn/cutepikachu/datawisemaster/SpringbootInitApplicationTests.java
+++ src/test/java/cn/cutepikachu/datawisemaster/SpringbootInitApplicationTests.java
@@ -1,14 +1,28 @@
 package cn.cutepikachu.datawisemaster;
 
+import cn.cutepikachu.datawisemaster.model.dto.chart.ChartAddRequest;
+import jakarta.annotation.Resource;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Set;
 
 @SpringBootTest
 class SpringbootInitApplicationTests {
 
+    @Resource
+    Validator validator;
+
     @Test
     void contextLoads() {
-
+        ChartAddRequest chartAddRequest = new ChartAddRequest();
+        chartAddRequest.setGoal("分析趋势");
+        chartAddRequest.setName("趋势图");
+        chartAddRequest.setChartType("line");
+        Set<ConstraintViolation<ChartAddRequest>> violationSet = validator.validate(chartAddRequest);
+        System.out.println(violationSet);
     }
 
 }
