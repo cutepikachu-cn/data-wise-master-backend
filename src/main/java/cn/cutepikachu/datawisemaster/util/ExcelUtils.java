21a5245e248a79b1ac98d09f3d38c9bcd30bdaba
2894462606 <2894462606@qq.com>
2024-05-07 17:14:33 +0800

    feat: excel 文件读取工具开发

---
+++ src/main/java/cn/cutepikachu/datawisemaster/util/ExcelUtils.java
@@ -0,0 +1,59 @@
+package cn.cutepikachu.datawisemaster.util;
+
+import cn.cutepikachu.datawisemaster.common.ResponseCode;
+import cn.cutepikachu.datawisemaster.exception.BusinessException;
+import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.core.util.StrUtil;
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.support.ExcelTypeEnum;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author 笨蛋皮卡丘
+ * @version 1.0
+ */
+public class ExcelUtils {
+    public static String excelToCSV(MultipartFile file) {
+        // File dataFile;
+        // try {
+        //     dataFile = ResourceUtils.getFile("classpath:data.xlsx");
+        // } catch (FileNotFoundException e) {
+        //     throw new RuntimeException(e);
+        // }
+        List<Map<Integer, String>> rows;
+        try {
+            rows = EasyExcel.read(file.getInputStream())
+                    .excelType(ExcelTypeEnum.XLSX)
+                    .sheet()
+                    .headRowNumber(0)
+                    .doReadSync();
+        } catch (Exception e) {
+            throw new BusinessException(ResponseCode.OPERATION_ERROR, "文件读取失败");
+        }
+        // System.out.println(rows);
+        if (CollectionUtil.isEmpty(rows)) {
+            return "";
+        }
+        StringBuilder dataStr = new StringBuilder();
+        // 表头
+        Map<Integer, String> head = rows.get(0);
+        String headRowStr = StrUtil.join(",", head.values().stream().filter(ObjectUtil::isNotEmpty).toList());
+        dataStr.append(headRowStr).append('\n');
+        // 数据
+        for (int i = 1; i < rows.size(); i++) {
+            Map<Integer, String> row = rows.get(i);
+            String rowStr = StrUtil.join(",", row.values().stream().filter(ObjectUtil::isNotEmpty).toList());
+            dataStr.append(rowStr).append('\n');
+        }
+        // System.out.println(dataStr);
+        return dataStr.toString();
+    }
+
+    public static void main(String[] args) {
+        excelToCSV(null);
+    }
+}
