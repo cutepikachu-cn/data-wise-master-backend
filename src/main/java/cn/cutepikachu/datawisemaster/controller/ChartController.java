40496507fb189bafdee042dc9f4d6efe283faa76
2894462606 <2894462606@qq.com>
2024-05-08 22:08:29 +0800

    feat: 完善图表生成接口
    图表生成接口调用 AI 服务生成结果;

--- src/main/java/cn/cutepikachu/datawisemaster/controller/ChartController.java
+++ src/main/java/cn/cutepikachu/datawisemaster/controller/ChartController.java
@@ -4,6 +4,7 @@ import cn.cutepikachu.datawisemaster.annotation.AuthCheck;
 import cn.cutepikachu.datawisemaster.common.BaseResponse;
 import cn.cutepikachu.datawisemaster.common.DeleteRequest;
 import cn.cutepikachu.datawisemaster.common.ResponseCode;
+import cn.cutepikachu.datawisemaster.manager.AiManager;
 import cn.cutepikachu.datawisemaster.model.dto.chart.ChartGenRequest;
 import cn.cutepikachu.datawisemaster.model.dto.chart.ChartQueryRequest;
 import cn.cutepikachu.datawisemaster.model.entity.Chart;
@@ -16,6 +17,7 @@ import cn.cutepikachu.datawisemaster.util.ExcelUtil;
 import cn.cutepikachu.datawisemaster.util.ResponseUtil;
 import cn.cutepikachu.datawisemaster.util.ThrowUtil;
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.extern.slf4j.Slf4j;
@@ -24,6 +26,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.util.List;
 
 /**
  * <p>
@@ -44,6 +47,9 @@ public class ChartController {
     @Resource
     private IUserService userService;
 
+    @Resource
+    private AiManager aiManager;
+
     /**
      * 生成图表请求
      *
@@ -51,38 +57,36 @@ public class ChartController {
      * @param chartGenRequest
      */
     @PostMapping("/gen")
-    public BaseResponse<?> genChart(@RequestPart MultipartFile dataFile,
+    public BaseResponse<ChartVO> genChart(@RequestPart MultipartFile dataFile,
                                     @Valid ChartGenRequest chartGenRequest) {
-        // 参数校验
-        String data = ExcelUtil.excelToCSV(dataFile);
-        String goal = chartGenRequest.getGoal();
-        String name = chartGenRequest.getName();
-        String chartType = chartGenRequest.getChartType();
-
-        // 用户输入
-        StringBuilder userInput = new StringBuilder();
-        // {"goal":"分析网站用户增长趋势","name":"用户增长趋势表","chartType":"line"}
-        userInput.append("你是一个数据分析师和前端开发专家，根据我提供的的分析目标和原始数据，输出分析图表和结论\n");
-        userInput.append("分析需求: \n").append(goal).append('\n');
-        // userInput.append("图表名称: \n").append(name).append('\n');
-        // userInput.append("图表类型: \n").append(chartType).append('\n');
-        userInput.append("原始数据: \n").append(data);
-        userInput
-                .append("请根据这两部分内容，按照以下指定格式生成内容（此外不要输出任何多余的开头、结尾、注释）\n")
-                .append("【【【【【\n")
-                .append("{前端 Echarts V5 的 option 配置对象js代码，合理地将数据进行可视化，不要生成任何多余的内容，比如注释}\n")
-                .append("【【【【【\n")
-                .append("{明确的数据分析结论、越详细越好，不要生成多余的注释}");
-
         Chart chart = new Chart();
-        chart.setData(data);
         BeanUtil.copyProperties(chartGenRequest, chart);
         User loginUser = userService.getLoginUser();
         chart.setUserId(loginUser.getId());
-        // boolean result = chartService.save(chart);
-        // ThrowUtils.throwIf(!result, ResponseCode.OPERATION_ERROR);
-        // System.out.println(userInput);
-        return ResponseUtil.success("添加图表成功", userInput);
+        String data = ExcelUtil.excelToCSV(dataFile);
+        chart.setData(data.trim());
+
+        String goal = chartGenRequest.getGoal();
+        String name = chartGenRequest.getName();
+        String chartType = chartGenRequest.getChartType();
+        // 拼接用户输入
+        // {"goal":"分析网站用户增长趋势","name":"用户增长趋势图","chartType":"line"}
+        String userInput = "分析需求: \n" + goal + '\n' +
+                "图表名称为" + name + '\n' +
+                "图表类型为" + chartType + '\n' +
+                "原始数据: \n" + data;
+        // AI 调用
+        String aiGenResult = aiManager.doChat(userInput);
+        List<String> splitResult = StrUtil.split(aiGenResult, "*****");
+        ThrowUtil.throwIf(splitResult.size() < 2, ResponseCode.OPERATION_ERROR, "生成错误");
+        String genChart = splitResult.get(1).trim();
+        String genResult = splitResult.get(2).trim();
+
+        chart.setGenChart(genChart);
+        chart.setGenResult(genResult);
+        boolean result = chartService.save(chart);
+        ThrowUtil.throwIf(!result, ResponseCode.OPERATION_ERROR);
+        return ResponseUtil.success(chart.toVO(ChartVO.class));
     }
 
     /**
