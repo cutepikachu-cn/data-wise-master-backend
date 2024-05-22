5b6fdffc8c06122cd84f717ae83685d0c2b8455a
2894462606 <2894462606@qq.com>
2024-05-22 22:07:59 +0800

    test: RabbitMQ 学习
    Work Queues：多消费者，Producer-Consumer 一对多

---
+++ src/test/java/cn/cutepikachu/datawisemaster/mq/MultiConsumer.java
@@ -0,0 +1,68 @@
+package cn.cutepikachu.datawisemaster.mq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author <a href="https://github.com/cutepikachu-cn">笨蛋皮卡丘</a>
+ * @version 1.0
+ * @since 2024-05-22 15:28:00
+ */
+public class MultiConsumer {
+
+    private static final String TASK_QUEUE_NAME = "task_queue";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        final Connection connection = factory.newConnection();
+
+        // 创建多个消费者
+        for (int i = 0; i < 3; i++) {
+            final Channel channel = connection.createChannel();
+            // 队列持久化
+            // durable: true
+            channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+            System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+            channel.basicQos(1);
+            int finalI = i;
+            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+
+                System.out.println(" [" + finalI + "] Received '" + message + "'");
+                try {
+                    // doWork(message);
+
+                    channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+                } catch (Exception e) {
+                    channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+                } finally {
+                    System.out.println(" [" + finalI + "] Done");
+                    channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+                }
+            };
+            // 开启消费监听
+            // autoAck: false 不自动确认
+            channel.basicConsume(TASK_QUEUE_NAME, false, deliverCallback, consumerTag -> {
+            });
+        }
+    }
+
+    private static void doWork(String task) {
+        for (char ch : task.toCharArray()) {
+            if (ch == '.') {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException _ignored) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+}
+
---
+++ src/test/java/cn/cutepikachu/datawisemaster/mq/MultiProducer.java
@@ -0,0 +1,41 @@
+package cn.cutepikachu.datawisemaster.mq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+/**
+ * @author <a href="https://github.com/cutepikachu-cn">笨蛋皮卡丘</a>
+ * @version 1.0
+ * @since 2024-05-22 15:20:00
+ */
+public class MultiProducer {
+    private static final String TASK_QUEUE_NAME = "task_queue";
+
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+
+            Scanner scanner = new Scanner(System.in);
+
+            while (scanner.hasNext()) {
+                String message = scanner.nextLine();
+
+                // 消息持久化
+                // MessageProperties.PERSISTENT_TEXT_PLAIN
+                channel.basicPublish("", TASK_QUEUE_NAME,
+                        MessageProperties.PERSISTENT_TEXT_PLAIN,
+                        message.getBytes(StandardCharsets.UTF_8));
+                System.out.println(" [x] Sent '" + message + "'");
+            }
+        }
+    }
+}
